ea66d709a790b333a98ad3493654c19b1124c414
ChiaChienChen <geeks.ajen@gmail.com>
2017-06-21 10:22:25 +0800

    sign id_token cookie after user login

--- src/core/passport/passport.js
+++ src/core/passport/passport.js
@@ -4,6 +4,7 @@ import { Strategy as LocalStrategy } from 'passport-local';
 
 /**
  * Sign in with our account
+ * 這邊只需要做驗證
  */
 passport.use(new LocalStrategy({
   usernameField: 'account',
@@ -12,7 +13,15 @@ passport.use(new LocalStrategy({
   session: false,
 }, (req, account, password, done) => {
   const task = graphql(req.schema, `{ login(account:"${account}",password:"${password}") { id,account,email } }`, { request: req })
-  .then(userAccount => done(null, userAccount.data.login))
+  .then((userAccount) => {
+    if (userAccount.data.login == null) throw new Error('userAccount not exist');
+    const user = {
+      id: userAccount.data.login.id,
+      account: userAccount.data.login.account,
+      email: userAccount.data.login.email,
+    };
+    done(null, user);
+  })
   .catch(err => done(err));
 
   return task;
--- src/server.js
+++ src/server.js
@@ -81,7 +81,10 @@ app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
 app.post('/login',
   passport.authenticate('local', { failureRedirect: '/login', session: false }),
   (req, res) => {
-    res.send('new hash(profile)store in cookie');
+    const expiresIn = 60 * 60 * 24 * 180; // 180 days
+    const token = jwt.sign(req.user, config.auth.jwt.secret, { expiresIn });
+    res.cookie('id_token', token, { maxAge: 1000 * expiresIn, httpOnly: true });
+    res.redirect('/');
   },
 );
 
